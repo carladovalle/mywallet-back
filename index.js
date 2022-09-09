25ecc76367aa5443214c28202b49412da0d32cd1
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-09-09 11:17:35 -0300

    feat: allows you to register

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 import express from "express";
 import cors from "cors";
 import chalk from "chalk";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import bcrypt from 'bcrypt';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Servidor ok");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+    db = mongoClient.db("mywallet");
 })
+
+app.post("/sign-up", async (req,res) => {
+
+    const {name, email, password, confirmPassword} = req.body;
+
+    const hashPassword = bcrypt.hashSync(password, 10);
+
+    try {
+        db.collection("users").insertOne({
+            name,
+            email,
+            password: hashPassword,
+            confirmPassword
+        });
+    } catch (error) {
+        console.error(error);
+        return res.send(500);
+    }
+    res.sendStatus(201);
+});
 
 app.listen(5000, () => {
     console.log(chalk.blue("Servidor rodando."));
